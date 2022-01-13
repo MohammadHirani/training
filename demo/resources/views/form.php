5eb6cb5e6e262dd83374f07785dda02dfa9b169f
Vaishali Kariya <vaishalikariya7@gmail.com>
2022-01-13 13:46:48 +0530

    Added

---
+++ demo/resources/views/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>reg.form</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        name:
+        <input type="text" id="name" name="name">
+        <br>
+
+    </form>
+
+</body>
+
+</html>
